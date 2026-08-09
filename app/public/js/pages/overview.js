3d084d14533ad3db1c22ca6c029c3590be8495c8
Muh Al Asfahani <asfah21@gmail.com>
2026-08-09 14:50:24 +0800

    [feat] fix overview

--- app/public/js/pages/overview.js
+++ app/public/js/pages/overview.js
@@ -1,5 +1,5 @@
 import { state } from '../state.js';
-import { getWitaDateString } from '../utils.js';
+import { getWitaDateString, getBusinessTimeParts } from '../utils.js';
 
 let attendanceChart = null;
 
